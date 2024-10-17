3e7a8e40cd6d7f4f9657354b08c22dc5b62687cd
Aleksandr Reshetnikov <ar@kolos.studio>
2024-10-18 01:15:50 +0300

    gohtml

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,8 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
+	"text/template"
 	"time-tracker/internal/config"
 )
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	layout := filepath.Join("web", "templates", "layout.gohtml")
+	tmplPath := filepath.Join("web", "templates", tmpl+".gohtml")
+	templates, err := template.ParseFiles(layout, tmplPath)
+	if err != nil {
+		log.Println("Error loading template " + tmplPath)
+		http.Error(w, "Error loading template "+tmpl, http.StatusInternalServerError)
+		return
+	}
+	err = templates.ExecuteTemplate(w, "layout", data)
+	if err != nil {
+		log.Println("Error rendering template " + tmplPath)
+		log.Println(err)
+		http.Error(w, "Error rendering template "+tmpl, http.StatusInternalServerError)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index", map[string]interface{}{
+		"Title": "Dashboard",
+	})
+}
+
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "signup", map[string]interface{}{
+		"Title": "Sign Up",
+	})
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
 	cfg, err := config.LoadConfig()
@@ -13,6 +48,17 @@ func main() {
 		log.Fatalf("Could not load config: %v", err)
 	}
 	fmt.Println(":" + cfg.Port)
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/dashboard", handler)
+	//http.HandleFunc("/projects", handler)
+	//http.HandleFunc("/projects/{project_id}", handler)
+	http.HandleFunc("/tasks", handler)
+	http.HandleFunc("/reports", handler)
+	http.HandleFunc("/login", handler)
+	http.HandleFunc("/signup", signupHandler)
+	http.HandleFunc("/profile", handler)
+	http.HandleFunc("/settings", handler)
 
 	log.Fatal(http.ListenAndServe(":"+cfg.Port, nil))
 }
