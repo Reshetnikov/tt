e2dea54f3cd9871574d47eba77385a4686ce5d44
Aleksandr Reshetnikov <ar@kolos.studio>
2024-11-07 10:50:01 +0300

    tty: true

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-air
-#air &
-#cd "$(dirname "$0")/../web" && npm install && npm run watch:css
-#wait
+air &
+cd "$(dirname "$0")/../web" && npm install && npm run watch:css
+# cd "$(dirname "$0")/../web" && npm install && npm run watch:css--loglevel=verbose
+# wait
