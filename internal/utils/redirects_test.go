93cf09a719e8639279a766dbb29c0052e399e407
Aleksandr Reshetnikov <ar@kolos.studio>
2024-12-04 13:36:15 +0300

    TestValidator

--- internal/utils/redirects_test.go
+++ internal/utils/redirects_test.go
@@ -11,21 +11,12 @@ import (
 
 // docker exec -it tt-app-1 go test -v ./internal/utils --tags=unit -cover -run TestRedirect.*
 func TestRedirectRoot(t *testing.T) {
-	// Создаем тестовый ResponseRecorder
 	w := httptest.NewRecorder()
-
-	// Создаем фиктивный HTTP-запрос
 	r := httptest.NewRequest(http.MethodGet, "/some-path", nil)
-
-	// Вызываем функцию редиректа
 	RedirectRoot(w, r)
-
-	// Проверяем статус-код ответа
 	if w.Code != http.StatusSeeOther {
 		t.Errorf("Expected status code %d, got %d", http.StatusSeeOther, w.Code)
 	}
-
-	// Проверяем заголовок Location
 	location := w.Header().Get("Location")
 	if location != "/" {
 		t.Errorf("Expected redirect to '/', got '%s'", location)
@@ -35,13 +26,10 @@ func TestRedirectRoot(t *testing.T) {
 func TestRedirectLogin(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/some-path", nil)
-
 	RedirectLogin(w, r)
-
 	if w.Code != http.StatusSeeOther {
 		t.Errorf("Expected status code %d, got %d", http.StatusSeeOther, w.Code)
 	}
-
 	location := w.Header().Get("Location")
 	if location != "/login" {
 		t.Errorf("Expected redirect to '/login', got '%s'", location)
@@ -51,13 +39,10 @@ func TestRedirectLogin(t *testing.T) {
 func TestRedirectDashboard(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/some-path", nil)
-
 	RedirectDashboard(w, r)
-
 	if w.Code != http.StatusSeeOther {
 		t.Errorf("Expected status code %d, got %d", http.StatusSeeOther, w.Code)
 	}
-
 	location := w.Header().Get("Location")
 	if location != "/dashboard" {
 		t.Errorf("Expected redirect to '/dashboard', got '%s'", location)
@@ -67,15 +52,11 @@ func TestRedirectDashboard(t *testing.T) {
 func TestRedirectSignupSuccess(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/some-path", nil)
-
 	testEmail := "test@example.com"
-
 	RedirectSignupSuccess(w, r, testEmail)
-
 	if w.Code != http.StatusSeeOther {
 		t.Errorf("Expected status code %d, got %d", http.StatusSeeOther, w.Code)
 	}
-
 	expectedLocation := "/signup-success?email=" + url.QueryEscape(testEmail)
 	location := w.Header().Get("Location")
 	if location != expectedLocation {
